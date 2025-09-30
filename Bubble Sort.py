57d5a1cbbed7b6e3777cd89b46b59f165106e886
Samarth Bagul <119740483+samarth-bagul@users.noreply.github.com>
2025-10-01 00:11:25 +0530

    Add files via upload

---
+++ Bubble Sort.py
@@ -0,0 +1,12 @@
+# Bubble Sort in Python
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+
+# Example
+arr = [64, 25, 12, 22, 11]
+print("Sorted:", bubble_sort(arr))
